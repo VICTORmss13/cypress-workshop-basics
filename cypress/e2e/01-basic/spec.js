648fd948d21af7c6912f20696002718634723b1a
Victor Mendes <victormendes13@gmail.com>
2024-02-03 20:13:19 -0300

    01-basic Solved

--- cypress/e2e/01-basic/spec.js
+++ cypress/e2e/01-basic/spec.js
@@ -14,7 +14,7 @@ it('loads', () => {
   // this assertion fails on purpose
   // can you fix it?
   // https://on.cypress.io/contains
-  cy.contains('h1', 'Todos App')
+  cy.contains('h1', 'todos')
 
   // can you write "cy.contains" using regular expression?
   // cy.contains('h1', /.../)
